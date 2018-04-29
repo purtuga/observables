a3507f8118e5279cd25f6fdfe89a1fc62044b31f
Paul T <paultavares1@gmail.com>
2018-04-29 09:14:56 -0400

    Latest built files

--- dist/observables.js
+++ dist/observables.js
@@ -536,8 +536,8 @@
                             var _arrCurrentProto$meth;
                             for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) args[_key] = arguments[_key];
                             var response = (_arrCurrentProto$meth = arrCurrentProto[method]).call.apply(_arrCurrentProto$meth, [ this ].concat(args));
-                            arr[OBSERVABLE_IDENTIFIER].dependents.notify();
-                            arr[OBSERVABLE_IDENTIFIER].watchers.notify();
+                            this[OBSERVABLE_IDENTIFIER].dependents.notify();
+                            this[OBSERVABLE_IDENTIFIER].watchers.notify();
                             return response;
                         }
                     });
